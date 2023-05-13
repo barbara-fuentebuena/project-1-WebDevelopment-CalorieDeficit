b17ce38eb9e39bfa812b069ed422877f9b9380c7
barbaraf <barbi.fb@hotmail.com>
2023-05-14 01:57:41 +0200

    js implemented

---
+++ js/cart.js
@@ -0,0 +1,191 @@
+//Create new product
+class Product {
+    constructor(id, name, price, type, img) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.img = img;
+        this.quantity = 1;
+    }
+}
+
+//Products available a the moment
+const shakerPink = new Product(1, "Shaker", 50, "Pink", "../img/shakers.png");
+const shakerBlue = new Product(2, "Shaker", 50, "Blue", "../img/shakers.png");
+const shakerGreen = new Product(3, "Shaker", 50, "Green", "../img/shakers.png");
+const shakerYellow = new Product(4, "Shaker", 50, "Yellow", "../img/shakers.png");
+const wheyProteinVanilla = new Product(10, "Whey Protein", 250, "Vanilla", "../img/whey-protein-banner.jpeg");
+const wheyProteinChocolate = new Product(11, "Whey Protein", 250, "Chocolate", "../img/whey-protein-banner.jpeg");
+const wheyProteinStrawberry = new Product(12, "Whey Protein", 250, "Strawberry", "../img/whey-protein-banner.jpeg");
+const bmiScaleWhite = new Product(20, "BMI Scale", 1500, "White", "../img/bmi-scales.png");
+const bmiScaleBlack = new Product(21, "BMI Scale", 1500, "Black", "../img/bmi-scales.png");
+
+let selectProduct = "";
+//array of product in the cart
+let cartItems = [];
+
+document.addEventListener("DOMContentLoaded", function() {
+    const colorSelect = document.getElementById("colorShaker");
+    const flavourSelect = document.getElementById("flavourWheyProtein")
+    const colorBMISelect = document.getElementById("colorBMIScale")
+    const addToCartBtn = document.getElementById("addToCartBtn");
+    const cartContainer = document.getElementById("cartContainer");
+
+    if (window.location.href.includes("shop-shakers.html")){
+        //event to select color for shakers and assign ID
+    colorSelect.addEventListener("change", function() {
+        const selectedColor = colorSelect.value;
+        if (selectedColor === "1") {
+        selectProduct = shakerPink;
+        selectProduct.id = 1;
+        } else if (selectedColor === "2") {
+        selectProduct = shakerBlue;
+        selectProduct.id = 2;
+        } else if (selectedColor === "3") {
+        selectProduct = shakerGreen;
+        selectProduct.id = 3;
+        } else if (selectedColor === "4") {
+        selectProduct = shakerYellow;
+        selectProduct.id = 4;
+        }
+    });
+    } else if (window.location.href.includes("shop-whey-protein.html")){
+        //event to select flavour for whey protein and assign ID
+    flavourSelect.addEventListener("change", function() {
+        const selectedFlavour = flavourSelect.value;
+        if (selectedFlavour === "5") {
+        selectProduct = wheyProteinVanilla;
+        selectProduct.id = 10;
+        } else if (selectedFlavour === "6") {
+        selectProduct = wheyProteinChocolate;
+        selectProduct.id = 11;
+        } else if (selectedFlavour === "7") {
+        selectProduct = wheyProteinStrawberry;
+        selectProduct.id = 12;
+        }
+    });
+    } else if (window.location.href.includes("shop-bmi-scales.html")){
+        //event to select color for the BMI Scale and assign ID
+    colorBMISelect.addEventListener("change", function() {
+        const selectedColor = colorBMISelect.value;
+        if (selectedColor === "8") {
+            selectProduct = bmiScaleWhite;
+            selectProduct.id = 20;
+            } else if (selectedColor === "9") {
+            selectProduct = bmiScaleBlack;
+            selectProduct.id = 21;
+            }
+        })
+    }
+    //create event to the button "ADD TO CART"
+    addToCartBtn.addEventListener("click", function() {
+        addToCart(selectProduct);
+        updateCart();
+    });
+    //function to add products to the cart
+    function addToCart(product) {
+        cartItems = localStorage.getItem("cartItems");
+        cartItems = cartItems ? JSON.parse(cartItems) : [];
+
+        const existingItem = cartItems.find(item => item.id === product.id);
+        if (existingItem) {
+        existingItem.quantity += 1;
+        } else {
+        cartItems.push(product);
+        }
+        
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    }
+    //function to show the updated cart
+    let itemPrice = 0;
+    let totalPrice = 0;
+    function updateCart() {
+        cartItems = localStorage.getItem("cartItems");
+        cartItems = cartItems ? JSON.parse(cartItems) : [];
+        
+        cartContainer.innerHTML = "";
+        
+        if (cartItems.length === 0) {
+        cartContainer.innerHTML = `<h6 id="noItems">No items in cart.</h6>`;
+        } else {
+            cartItems.forEach(item => {
+                const cartItem = document.createElement("div");
+                cartItem.classList.add("cart-item");
+                cartItem.innerHTML = `
+                        <div class="item-info">
+                            <li class="item-details">
+                                <h5 class="item-name">${item.name} - ${item.type}</h5>
+                                <h6 class="item-price">DKK ${item.price}</h6>
+                                <h6 class="item-quantity">Quantity: ${item.quantity}</h6>  
+                            </li>
+                            <button class="remove-item" data-id="${item.id}"> X </button>
+                        </div>`;
+                //update total amount $$
+                itemPrice = item.price * item.quantity;
+                totalPrice += itemPrice;
+                cartContainer.appendChild(cartItem);
+            });
+            //show total amount $$ of the cart
+            const totalElement = document.createElement("p");
+            totalElement.classList.add("cart-total");
+            totalElement.textContent = "Total: DKK " + totalPrice;
+            cartContainer.appendChild(totalElement);
+            //event to button to remove product from the cart
+            const removeButtons = document.getElementsByClassName("remove-item");
+            [...removeButtons].forEach(button => {
+                button.addEventListener("click", function() {
+                const itemId = parseInt(button.dataset.id);
+                removeItem(itemId);
+                updateCart();
+                });
+            });
+        }
+    }
+    //call to updateCart function
+    updateCart();
+    //checkout function
+    const checkOut = document.getElementById("checkOut")
+    checkOut.addEventListener("click", ()=>{
+        if(cartItems.length === 0){
+            Swal.fire({
+                title: "Your cart is empty.",
+                icon: "error",
+            })
+        }else{
+            Swal.fire({
+                title: "Purchase succesfull",
+                text: `You will receive your order soon!`,
+                icon: "success",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                clearCart();
+            }})
+        }
+    })
+    //function to remove items from the cart
+    function removeItem(itemId) {
+        cartItems = localStorage.getItem("cartItems");
+        cartItems = cartItems ? JSON.parse(cartItems) : [];
+
+        for (let i = 0; i < cartItems.length; i++) {
+            if (cartItems[i].id === itemId) {
+                if (cartItems[i].quantity > 1) {
+                    cartItems[i].quantity -= 1;
+                } else {
+                    cartItems.splice(i, 1);
+                }
+                break;
+            }
+        }
+
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
+        updateCart();
+    }
+
+    //function to empty the cart
+    function clearCart() {
+    localStorage.removeItem("cartItems");
+    updateCart();
+    }
+});
---
+++ js/main.js
@@ -0,0 +1,140 @@
+
+//render calculate button to calculate the IBM
+const calculateButton = document.getElementById("calculateButton");
+
+//clear previous messages:
+    //result message:
+    let message = document.getElementById("resultTotal")
+    message.innerHTML = ""
+    resetButton.className = "noDisplay"
+
+//action when the calculate button is clicked
+calculateButton.addEventListener("click", ()=>{
+    
+    //get values - DOM
+    let emailUser = document.getElementById("emailUser").value
+    let gender = document.querySelector("#genderInput").value
+    const age = parseInt(document.getElementById("ageInput").value)
+    const height = parseInt(document.getElementById("heightInput").value)
+    const weight = parseInt(document.getElementById("weightInput").value)
+    let activityLevel = document.querySelector("#activityLevelInput").value
+
+    //initialize isValid to true
+    let isValid = true; 
+
+    //function for errorMessage with toastify
+    const errorMessageToastify = (message) =>{
+        Toastify({
+            text: message,
+            duration: 4000,
+            gravity: "top",
+            position: "right",
+            style: {
+                background: "red",
+            }
+        }).showToast()
+    }
+
+    //rules for values
+    if((isNaN(age)) || ((age < 0)) || (age == null)){
+        isValid = false;
+        errorMessageToastify("You must insert a positive number for age.")
+    }else if(age < 18){
+        isValid = false;
+        errorMessageToastify("You must to be over 18 years old to access to the calculator.")
+    }else if((isNaN(weight)) || (weight < 0) || (weight == null)){
+        isValid = false;
+        errorMessageToastify("You must insert a positive number for weight.")
+    }else if((isNaN(height)) || (height < 0) || (height == null)){
+        isValid = false;
+        errorMessageToastify("You must insert a positive number for height.")
+    }else if(emailUser == "" || emailUser.includes("@" && ".") == false){
+        isValid = false;
+        errorMessageToastify("You must insert a valid email.")  
+    }else if(gender == "none" || activityLevel == "none"){
+        isValid = false;
+        errorMessageToastify("Make sure you select your gender and activity level.")
+    }
+
+    //if there are no errors in the inputs:
+    if (isValid) {
+        //choose gender value
+        if(gender == "female"){
+            gender = -161
+        }else if(gender == "male"){
+            gender = 5
+        }else if(gender == "other"){
+            gender = -80
+        }
+        //choose activity value
+        if(activityLevel == "1"){
+            activityLevel = 500
+        }else if(activityLevel == "2"){
+            activityLevel = 300
+        }else if(activityLevel == "3"){
+            activityLevel = 150
+        }else if(activityLevel == "4"){
+            activityLevel = 0
+        }
+
+        //calculator
+        let result = parseInt(((10 * weight) + (6.65 * height) - (5 * age) + gender)+activityLevel)
+        //show result
+        let message = document.getElementById("resultCal")
+        message.innerHTML = `
+        <h2>Your TBM is ${result} calories per day</h2>
+        <h4>It means that you should consume ${result} calories as maximum per day if you want to lose weight.</h4>
+        <p>*Consult with a doctor if you are elegible for a calorie deficit programm.</p>
+        `
+        message.className = "resultMessage"
+        //show reset button to reset the calculator
+        let resetButton = document.getElementById("resetButton")
+        resetButton.innerHTML = `RESET ↺`
+        resetButton.className = "resetButton"
+        resetButton.addEventListener("click", ()=>{
+            const reset = () =>{
+                document.getElementById("emailUser").value = ""
+                document.querySelector("#genderInput").value = "none"
+                document.getElementById("ageInput").value = ""
+                document.getElementById("heightInput").value = ""
+                document.getElementById("weightInput").value = ""
+                document.querySelector("#activityLevelInput").value = "none"
+                message.innerHTML = "Your TBM is ---"
+                resetButton.innerHTML = ``
+                resetButton.className = "noDisplay"
+            }
+            reset()
+        })
+        
+        //clear input values after calculation
+        document.getElementById("emailUser").value = ""
+        document.querySelector("#genderInput").value = "none"
+        document.getElementById("ageInput").value = ""
+        document.getElementById("heightInput").value = ""
+        document.getElementById("weightInput").value = ""
+        document.querySelector("#activityLevelInput").value = "none"
+
+        //create new user object
+        class User{
+            constructor(emailUser, gender, age, height, weight, activityLevel){
+                this.emailUser = emailUser;
+                this.gender = gender;
+                this.age = age;
+                this.height = height;
+                this.weight = weight;
+                this.activityLevel = activityLevel
+            }
+        }
+        const newUser = new User (emailUser, gender, age, height, weight, activityLevel)
+
+        //add user object to usersArray
+        usersArray.push(newUser)
+
+        //save usersArray to localStorage
+        localStorage.setItem("usersArray", JSON.stringify(usersArray))
+    }
+})
+
+//get usersArray from localStorage 
+const usersArray = JSON.parse(localStorage.getItem("usersArray")) || []
+
