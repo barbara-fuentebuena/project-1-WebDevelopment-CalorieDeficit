a637838066bbf78210909f0d359fa714140c49f3
barbaraf <barbi.fb@hotmail.com>
2023-05-14 21:29:05 +0200

    new version of readme

--- README.md
+++ README.md
@@ -2,23 +2,23 @@ Calorie Deficit (blog) - README
 ¡Bienvenido/a a Calorie Deficit!
 
 Descripción del proyecto
-Calorie deficit es un sitio web diseñado para ayudarte a alcanzar tus objetivos de estilo de vida saludable. Nuestro objetivo es brindarte recursos útiles y prácticos para que puedas llevar una vida activa y nutritiva.
+Calorie Deficit es mi primer proyecto de JavaScript, el cual originalmente fue desarrollado como parte de un curso de desarrollo web y luego reutilizado para implementar las funcionalidades en JavaScript. El sitio web está diseñado para ayudarte a alcanzar tus objetivos de estilo de vida saludable, brindándote recursos útiles y prácticos para llevar una vida activa y nutritiva.
 
 El proyecto está orientado principalmente al mercado danés, pero puede ser utilizado por personas de todo el mundo. Nuestro contenido está en inglés, lo que lo hace accesible para una audiencia global interesada en adoptar un estilo de vida saludable.
 
 Características principales
 El proyecto consta de las siguientes características principales:
 
-Calculadora de Déficit Calórico: Te ofrecemos una herramienta de cálculo de déficit calórico que te ayudará a determinar la cantidad de calorías que necesitas consumir diariamente para alcanzar tus objetivos de pérdida de peso o mantenimiento. Simplemente ingresa tu información personal y la calculadora te proporcionará los resultados.
+Calculadora de Déficit Calórico: Herramienta de cálculo de déficit calórico que te ayudará a determinar la cantidad de calorías que necesitas consumir diariamente para alcanzar tus objetivos de pérdida de peso o mantenimiento. Simplemente ingresa tu información personal y la calculadora te proporcionará los resultados.
 
-Recetas para alcanzar tus objetivos: En nuestro blog, encontrarás una amplia variedad de recetas saludables y deliciosas diseñadas específicamente para ayudarte a alcanzar tus objetivos de estilo de vida fit. Descubre opciones de desayuno, almuerzo, cena, snacks y postres que se adaptan a tus necesidades.
+Recetas para alcanzar tus objetivos: Amplia variedad de recetas saludables y deliciosas diseñadas específicamente para ayudarte a alcanzar tus objetivos de estilo de vida fit. 
 
-Mini tienda: Además de las recetas, también ofrecemos una selección de productos relacionados con el estilo de vida saludable. En nuestra tienda, podrás adquirir utensilios de cocina, ingredientes especiales y otros productos que te ayudarán en tu viaje hacia una vida más saludable.
+Mini tienda: Selección de productos relacionados con el estilo de vida saludable. 
 
 Estructura del proyecto
 El proyecto está estructurado en diferentes secciones:
 
-Index: Aquí encontrarás el formulario de suscripción a nuestro boletín de noticias (newsletter). Este formulario está implementado utilizando JavaScript para realizar validaciones y mostrar mensajes de éxito o error al enviar el formulario.
+Index: Aquí encontrarás el formulario de suscripción al newsletter. Este formulario está implementado utilizando JavaScript para realizar validaciones y mostrar mensajes de éxito o error al enviar el formulario.
 
 Calculator: En esta sección, tendrás acceso a la calculadora de déficit calórico. Puedes ingresar tu información personal y obtener una estimación de las calorías diarias que debes consumir para alcanzar tus objetivos.
 
@@ -27,9 +27,8 @@ Shop: En la sección de tienda, podrás explorar los productos disponibles para 
 Pendientes del proyecto
 A pesar de los avances realizados, aún hay algunos aspectos que quedan pendientes en el proyecto:
 
-Activar la barra de búsqueda: Actualmente, la función de búsqueda de recetas no está implementada en el proyecto. Estamos trabajando en ello y pronto podrás disfrutar de esta función para buscar recetas específicas.
-Agradecemos tu interés en nuestro proyecto y esperamos que encuentres útil y motivador el contenido que ofrecemos. Si tienes alguna pregunta o sugerencia, no dudes en contactarnos.
+Activar la barra de búsqueda: Actualmente, la función de búsqueda de recetas no está implementada en el proyecto. 
 
-¡Disfruta de tu viaje hacia un estilo de vida fit y saludable!
+Muchas gracias!
 
-Equipo de Calorie Deficit
+Calorie deficit (Bárbara 😁)
--- js/shipping.js
+++ js/shipping.js
@@ -1,3 +1,61 @@
+//function for errorMessage with toastify
+const errorMessageToastifyShipping = (message) =>{
+    Toastify({
+        text: message,
+        duration: 2000,
+        gravity: "top",
+        position: "right",
+        style: {
+            background: "red",
+            color: "white",
+        },
+    }).showToast()
+}
+
+if (window.location.href.includes("shop-shakers.html")){
+    //render the calculate shipping button
+const calculateShippingBtn = document.getElementById("calculateShippingBtn");
+
+//add event listener to the calculate shipping button
+calculateShippingBtn.addEventListener("click", async function() {
+    //get the postal code input element and its value
+    const postalCodeInput = document.getElementById("postalCodeInput");
+    const postalCode = postalCodeInput.value;
+
+    //get the country select element and its value
+    const countrySelect = document.getElementById("countrySelect");
+    const country = countrySelect.value;
+
+    let shippingCost = 0; 
+
+    try {
+        //check the selected country
+        if (country === "denmark") {
+            //fetch postal code data from the API
+            const response = await fetch(`https://api.zippopotam.us/DK/${postalCode}`);
+            if (!response.ok) {
+                throw new Error("Invalid postal code for Denmark.");
+            }
+            const data = await response.json();
+
+            //check if the postal code is valid for Denmark
+            if (data.places && data.places.length > 0) {
+                shippingCost = 0; 
+            } else {
+                throw new Error("Invalid postal code for Denmark.");
+            }
+        } else if (country === "europe") {
+            shippingCost = 50; 
+        } else {
+            shippingCost = 100; 
+        }
+
+        // Display the shipping cost
+        const shippingCostElement = document.getElementById("shippingCost");
+        shippingCostElement.textContent = `DKK ${shippingCost}`;
+    } catch (error) {
+        //reset the shippingCost
+        shippingCost = ""; 
 
         //show the error message only if the country is Denmark and the code doesn't exist in the API
         if (country === "denmark") {
@@ -6,6 +64,119 @@
     } finally {
         //update the shipping cost in case of error
         const shippingCostElement = document.getElementById("shippingCost");
-        shippingCostElement.textContent = shippingCost === "" ? "" : `$${shippingCost}`;
+        shippingCostElement.textContent = shippingCost === "" ? "" : `DKK ${shippingCost}`;
     }
 });
+}else if(window.location.href.includes("shop-whey-protein.html")){
+    //render the calculate shipping button
+const calculateShippingBtn2 = document.getElementById("calculateShippingBtn2");
+
+//add event listener to the calculate shipping button
+calculateShippingBtn2.addEventListener("click", async function() {
+    //get the postal code input element and its value
+    const postalCodeInput = document.getElementById("postalCodeInput");
+    const postalCode = postalCodeInput.value;
+
+    //get the country select element and its value
+    const countrySelect = document.getElementById("countrySelect");
+    const country = countrySelect.value;
+
+    let shippingCost = 0; 
+
+    try {
+        //check the selected country
+        if (country === "denmark") {
+            //fetch postal code data from the API
+            const response = await fetch(`https://api.zippopotam.us/DK/${postalCode}`);
+            if (!response.ok) {
+                throw new Error("Invalid postal code for Denmark.");
+            }
+            const data = await response.json();
+
+            //check if the postal code is valid for Denmark
+            if (data.places && data.places.length > 0) {
+                shippingCost = 0; 
+            } else {
+                throw new Error("Invalid postal code for Denmark.");
+            }
+        } else if (country === "europe") {
+            shippingCost = 50; 
+        } else {
+            shippingCost = 100; 
+        }
+
+        // Display the shipping cost
+        const shippingCostElement = document.getElementById("shippingCost");
+        shippingCostElement.textContent = `DKK ${shippingCost}`;
+    } catch (error) {
+        //reset the shippingCost
+        shippingCost = ""; 
+
+        //show the error message only if the country is Denmark and the code doesn't exist in the API
+        if (country === "denmark") {
+            errorMessageToastifyShipping(error.message);
+        }
+    } finally {
+        //update the shipping cost in case of error
+        const shippingCostElement = document.getElementById("shippingCost");
+        shippingCostElement.textContent = shippingCost === "" ? "" : `DKK ${shippingCost}`;
+    }
+});
+}else if(window.location.href.includes("shop-bmi-scales.html")){
+    //render the calculate shipping button
+const calculateShippingBtn3 = document.getElementById("calculateShippingBtn3");
+
+//add event listener to the calculate shipping button
+calculateShippingBtn3.addEventListener("click", async function() {
+    //get the postal code input element and its value
+    const postalCodeInput = document.getElementById("postalCodeInput");
+    const postalCode = postalCodeInput.value;
+
+    //get the country select element and its value
+    const countrySelect = document.getElementById("countrySelect");
+    const country = countrySelect.value;
+
+    let shippingCost = 0; 
+
+    try {
+        //check the selected country
+        if (country === "denmark") {
+            //fetch postal code data from the API
+            const response = await fetch(`https://api.zippopotam.us/DK/${postalCode}`);
+            if (!response.ok) {
+                throw new Error("Invalid postal code for Denmark.");
+            }
+            const data = await response.json();
+
+            //check if the postal code is valid for Denmark
+            if (data.places && data.places.length > 0) {
+                shippingCost = 0; 
+            } else {
+                throw new Error("Invalid postal code for Denmark.");
+            }
+        } else if (country === "europe") {
+            shippingCost = 50; 
+        } else {
+            shippingCost = 100; 
+        }
+
+        // Display the shipping cost
+        const shippingCostElement = document.getElementById("shippingCost");
+        shippingCostElement.textContent = `DKK ${shippingCost}`;
+    } catch (error) {
+        //reset the shippingCost
+        shippingCost = ""; 
+
+        //show the error message only if the country is Denmark and the code doesn't exist in the API
+        if (country === "denmark") {
+            errorMessageToastifyShipping(error.message);
+        }
+    } finally {
+        //update the shipping cost in case of error
+        const shippingCostElement = document.getElementById("shippingCost");
+        shippingCostElement.textContent = shippingCost === "" ? "" : `DKK ${shippingCost}`;
+    }
+});
+}
+
+
